59fabcc4eaedb1732f98f9f907a6c66c11494dbc
AYPΞXΞ <Aypexe@gmail.com>
2025-12-11 23:40:19 -0500

    Update type.rb

--- Casks/type.rb
+++ Casks/type.rb
@@ -1,6 +1,6 @@
 cask "type" do
   version "b1.0"
-  sha256 "sha256:52b341b6382cb7b80aa5f300a5e5e12214246be42a20291ceba2a3b5e677342f"
+  sha256 "52b341b6382cb7b80aa5f300a5e5e12214246be42a20291ceba2a3b5e677342f"
 
   url "https://github.com/Aypexe/type/releases/download/b1.0/Type.dmg"
   name "Type"
