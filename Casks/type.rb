80fa52ddf52f358603a55847042fb5ce78a5d401
AYPΞXΞ <Aypexe@gmail.com>
2025-12-11 23:44:25 -0500

    Update SHA256 checksum for Type cask

--- Casks/type.rb
+++ Casks/type.rb
@@ -1,6 +1,6 @@
 cask "type" do
   version "b1.0"
-  sha256 "52b341b6382cb7b80aa5f300a5e5e12214246be42a20291ceba2a3b5e677342f"
+  sha256 "c541ab6f41b94eeda3cc1915ce25ab7ebe2c19519e2027048c39b2cba3aa92f1"
 
   url "https://github.com/Aypexe/type/releases/download/b1.0/Type.dmg"
   name "Type"
